e8a8e83943696a3eb2c2d5fb7bd9552cf44da8f2
Johannes Qvarford <jq.email+github@pm.me>
2022-04-03 10:47:56 +0200

    Can proxy to arbitrary url.

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,36 @@ mod parsing;
 mod atom_parser;
 mod xml_tree;
 
-fn main() -> Result<(), xmltree::ParseError> {
-    use xmltree::Element;
-    // use std::fs::File;
-    
-    let data: &'static str = r##"
-    <?xml version="1.0" encoding="UTF-8"?>
-    <TODO/>"##;
-    
-    let names_element = Element::parse(data.as_bytes())?;
-    
-    println!("{:#?}", names_element);
-    return Ok(())
-    //{
-        // get first `name` element
-        //let name = names_element.get_mut_child("name").expect("Can't find name element");
-        //name.attributes.insert("suffix".to_owned(), "mr".to_owned());
-    //}
-    // names_element.write(File::create("result.xml")?)
+#[macro_use] extern crate serde_derive;
+use actix_web::{get, web, App, HttpServer, Responder};
+use reqwest;
+use std::error::Error;
+
+#[get("/{id}/{name}/index.html")]
+async fn index(params: web::Path<(u32, String)>) -> impl Responder {
+    let (id, name) = params.into_inner();
+    format!("Hello {}! id:{}", name, id)
+}
+
+#[derive(Deserialize)]
+struct InvidiousProxyQuery {
+    proxied: String
+}
+
+#[get("/feeds/invidious-proxy")]
+async fn invidious_proxy(q: web::Query<InvidiousProxyQuery>) -> Result<String, Box<dyn Error>> {
+    let body = reqwest::get(q.proxied.clone())
+        .await?
+        .text()
+        .await?;
+    Ok(body)
+    //format!("hello {}", q.proxied)
+}
+
+#[actix_web::main] // or #[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index).service(invidious_proxy))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
