473f574e1ec0f462c29fb1e72dfb974cd3c3714c
Johannes Qvarford <jq.email+github@pm.me>
2022-04-09 09:12:04 +0200

    Cleaned up element->string conversion.

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,13 @@ mod atom_parser;
 mod xml_tree;
 
 #[macro_use] extern crate serde_derive;
+use crate::xml_tree::write_element_to_string;
 use actix_web::web::Buf;
-use actix_web::{get, web, App, HttpServer, Responder, HttpResponse};
+use actix_web::{get, web, App, HttpServer, Responder};
 use reqwest;
 use std::error::Error;
 use parsing::Parser;
 use anyhow;
-use bytes::BufMut;
-use tempfile::{tempdir};
-use md5::{Md5, Digest};
-use std::fs::{read_to_string, File};
-use std::str;
-use std::path::{Path, PathBuf};
-use hex;
 
 #[get("/{id}/{name}/index.html")]
 async fn index(params: web::Path<(u32, String)>) -> impl Responder {
@@ -40,19 +34,9 @@ async fn invidious_proxy(q: web::Query<InvidiousProxyQuery>) -> Result<String, B
     let parser = atom_parser::AtomParser{};
     let feed = parser.parse_feed(tree).map_err(anyhow::Error::msg)?;
     let response_tree = parser.serialize_feed(feed);
-    println!("{:?}", response_tree);
 
-    let dir = tempdir()?;
-    let mut hasher = Md5::new();
-    hasher.update(q.proxied.as_bytes());
-    let v: Vec<_> = hasher.finalize().into_iter().collect();
-    let filename = hex::encode(&v[..]);
-    let path_buf: PathBuf = dir.path().join(filename);
-    response_tree.write(File::create(path_buf.as_path())?)?;
-    let response_body = read_to_string(path_buf.as_path())?;
-
+    let response_body = write_element_to_string(&response_tree, &q.proxied)?;
     Ok(response_body)
-    //format!("hello {}", q.proxied)
 }
 
 #[actix_web::main] // or #[tokio::main]
